3561a339e6e7bcb6c1308760b455af043d065a19
Utsavkc789 <77692993+Utsavkc789@users.noreply.github.com>
2021-01-25 03:12:01 -0600

    Add files via upload

---
+++ Helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string user_input;
+    cout << "Enter your name ,Please:\n";
+    getline(std::cin,user_input);
+    
+    cout << "Hello " <<user_input<<"." <<" Thank You!";
+    return 0;
+    
+}
