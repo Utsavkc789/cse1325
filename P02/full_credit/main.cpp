a8ff4790cc51c8a86d5b3fe01a998d507d7c5e87
Utsavkc789 <utsav.kc@mavs.uta.edu>
2021-02-02 01:52:01 -0600

    Second_Assignment

---
+++ P02/full_credit/main.cpp
@@ -0,0 +1,22 @@
+#include "pet.h"
+
+int main()
+{
+    std::vector<Pet> pet_list;
+    Pet name1(BRUNO, DOG, 6);
+    Pet name2(STINKY, CAT, 7);
+    Pet name3(BOB, RABBIT, 8);
+    Pet name4(FAST, HORSE, 9);
+
+    pet_list.push_back(name1);
+    pet_list.push_back(name2);
+    pet_list.push_back(name3);
+    pet_list.push_back(name4);
+
+    for (int i = 0; i < pet_list.size(); i++)
+    {
+        std::cout << pet_list[i].to_string() << std::endl;
+    }
+
+    return 0;
+}
---
+++ P02/full_credit/pet.cpp
@@ -0,0 +1,51 @@
+ #include "pet.h"
+ 
+ 
+ std::string to_string(animal Animal)
+{
+
+    switch (Animal)
+    {
+
+    case DOG:
+        return "Dog";
+    case CAT:
+        return "Cat";
+    case RABBIT:
+        return "Rabbit";
+    case HORSE:
+        return "Horse";
+    }
+
+    return "Wrong animal";
+}
+
+std::string to_name(string a)
+{
+    switch (a)
+    {
+
+    case BRUNO:
+        return "Bruno";
+    case STINKY:
+        return "Stinky";
+    case BOB:
+        return "Bob";
+    case FAST:
+        return "Fast";
+    }
+    return "No such name";
+}
+
+    
+    Pet::Pet(string name, animal Animal, double age)
+    {
+        this->name = name;
+        this->Animal = Animal;
+        this->age = age;
+    }
+
+    std::string Pet::to_string()
+    {
+        return ::to_name(name) + " is a " + ::to_string(Animal) + " age " + std::to_string(age) +"."+"\n";
+    }
