673830d01168fb8056035f53c84ad0d80340239b
Utsavkc789 <utsav.kc@mavs.uta.edu>
2021-02-25 01:56:34 -0600

    Final one

--- P04/full_credit/main.cpp
+++ P04/full_credit/main.cpp
@@ -20,33 +20,13 @@ int main() {
     a.add_card("A block of memory associated with a symbolic name that contains an object instance or aprimitive data value","Variable");
     a.add_card("A short string representing a mathematical, logical, or machine control action","Operator");
     a.add_card("A method that returns the value of a private variable","Getter");
-    a.add_card("A method that changes the value of a private variable","Setter ");
+    a.add_card("A method that changes the value of a private variable","Setter");
     
     a.add_false_answer("Base class");
     a.add_false_answer("Override");
     a.add_false_answer("Namespace");
     
-    Card card0{"Reuse and extension of fields and method implementation from another class","Inheritance"};
-    Card card1{" A data type that can typically be handled directly by the underlying hardware","Primitive type"};
-    Card card2{"A data type consisting of a fixed set of constant values called enumerators","Enumerated type"};
-    Card card3{"A template encapsulating data and code that manipulates it","Class"};
-    Card card4{"An encapsulated bundle of data and code","Instance"};
-    Card card5{"An instance of a class containing a set of encapsulated data and associated methods","Object"};
-    Card card6{"A block of memory associated with a symbolic name that contains an object instance or aprimitive data value","Variable"};
-    Card card7{"A short string representing a mathematical, logical, or machine control action","Operator"};
-    Card card8{"A method that returns the value of a private variable","Getter"};
-    Card card9{"A method that changes the value of a private variable","Setter"};
-    Card _card{"",""};
-        cards.push_back(card0);
-        cards.push_back(card1);
-        cards.push_back(card2);
-        cards.push_back(card3);
-        cards.push_back(card4);
-        cards.push_back(card5);
-        cards.push_back(card6);
-        cards.push_back(card7);
-        cards.push_back(card8);
-        cards.push_back(card9);
+    Card _card={"",""};
         
     std::cout << "Select the number of term for each defination (-1 to exit)\n" << std::endl;
     ans=a.options();
