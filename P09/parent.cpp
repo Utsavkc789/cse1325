c03ed4f773e1b68831234a41e7e1f48359f7d657
Utsavkc789 <utsav.kc@mavs.uta.edu>
2021-04-19 15:41:20 -0500

    The Final one

--- P09/parent.cpp
+++ P09/parent.cpp
@@ -32,10 +32,6 @@ void Parent::add_student(Student& student) {
     _students.push_back(&student);
 }
 
-int Parent::students() {return _students.size();}
-Student& Parent::student(int index) {
-    return *(_students.at(index));
-}
 
 std::string Parent::full_info() const {
     std::string info = Person::full_info() + " - Students: ";
--- P09/parent.h
+++ P09/parent.h
@@ -8,17 +8,22 @@
 class Student;
 
 class Parent : public Person {
+  private:
+    std::vector<Student*> _students;
+     typedef std::vector<Student*> Students;
+  
   public:
     using Person::Person;
     void save(std::ostream& ost) override;
     void save_aggregates(std::ostream& ost);
     void load_aggregates(std::istream& ist, const std::map<std::string, Student*>& students);
     void add_student(Student& student);
-    int students();
-    Student& student(int index);
     std::string full_info() const override;
-  private:
-    std::vector<Student*> _students;
+    typedef Students::iterator iterator;
+    typedef Students::const_iterator const_iterator;
+    iterator begin() {return _students.begin();}
+    iterator end() {return _students.end();}
+  
 };
 
 #endif
