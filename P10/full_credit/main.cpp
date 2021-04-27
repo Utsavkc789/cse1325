d4335e7785dbc589ab6f50bfd43f7087c1f0fa0b
Utsavkc789 <utsav.kc@mavs.uta.edu>
2021-04-27 07:56:32 -0500

    Final

--- P10/full_credit/main.cpp
+++ P10/full_credit/main.cpp
@@ -1,52 +1,68 @@
 #include "puzzle.h"
 #include "solver.h"
-
 #include <iostream>
 #include <fstream>
 #include <vector>
-
 #include <algorithm>  // std::sort
+#include <thread>
+#include <mutex>
+std::mutex m;
+
+int const num_threads = 2 ;
+
+
+
+void puzzle_solver(std::vector<Puzzle> puzzles,std::vector<Solution>& solutions){
+        for(auto puzzle: puzzles){
+            for(std::string word:puzzle){
+                Solver solver{puzzle};
+                Solution s = solver.solve(word);
+                m.lock();
+                solutions.push_back(s);
+                m.unlock();
+                }}}
+
 
 int main(int argc, char* argv[]) {
 
     // Load the puzzles
-    std::vector<Puzzle> puzzles;
-    int num_threads = 0;
-    num_threads = atoi(argv[1]);
-    for(int i=2; i<argc; ++i) {
-        try {
-            std::string filename = std::string{argv[i]};
+    typedef std::vector<Puzzle> P;
+    
+    std::vector<P> puzzles;
+    
+    for(int i=1; i<argc; i++){
+        try{
+            std::string filename = std::string(argv[i]);
             std::ifstream ifs{filename};
-            puzzles.push_back(Puzzle{filename, ifs});
-        } catch(std::exception& e) {
-            std::cerr << "Unable to open " << argv[i] << ": " 
-                      << e.what() << std::endl;
-        }
-    }
-
-    // Exit if one or more puzzles failed to load
-    if((argc-1) != puzzles.size()) exit(-1);     
-    
-    // ----- All data loaded -----
-    
-    // Capture the solutions in a vector 
-    std::vector<Solution> solutions;
-
-    // Loop through all of the loaded puzzles
-    for(Puzzle& puzzle : puzzles) {
-
-        // Find each word in the current puzzle
-        for(std::string word : puzzle) {
-        
-            // Instance a Solver, and use it to solve for the word
-            Solver solver{puzzle};
-            Solution s = solver.solve(word);
-            solutions.push_back(s);
-        }
-    }
-    
-    std::sort(solutions.begin(), solutions.end());
-    for(Solution& s : solutions) std::cout << s << std::endl;
+            
+            if(puzzles.size() < num_threads){
+                P sub;
+                sub.push_back(Puzzle{filename,ifs});
+                puzzles.push_back(sub);
+                }
+                
+             else{
+                puzzles.at(i%num_threads).push_back(Puzzle{filename,ifs});
+                }
+                }
+                
+          catch(std::exception& e){
+                std::cerr << "Unable to open the file" << argv[i] << ": " << e.what() << std::endl;
+                }
+                }
+                     
+          std::array<std::thread,num_threads> _threads;
+          std::vector<Solution> solutions;
+          
+          for(int i=0; i<num_threads; i++) _threads[i] = std::thread{puzzle_solver,puzzles.at(i),std::ref(solutions)};
+          
+          for(std::thread& t: _threads)
+          t.join();
+          
+          std::sort(solutions.begin(),solutions.end());
+          for(Solution& s:solutions) std::cout << s << std::endl;
+           
+                 
 }
     
     
